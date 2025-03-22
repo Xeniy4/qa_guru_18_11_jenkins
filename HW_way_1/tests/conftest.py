bc165f6becef7f79ed4880647f2332af79cb8518
Goryaeva Kseniya <goryaeva_ks@mail.ru>
2025-03-22 19:04:33 +0300

    change fixture conftest.py

--- HW_way_1/tests/conftest.py
+++ HW_way_1/tests/conftest.py
@@ -9,71 +9,71 @@ from dotenv import load_dotenv
 from HW_way_1.Utils import attach
 
 
-# @pytest.fixture
-# def selenoid():
-#     options = Options()
-#     selenoid_capabilities = {
-#         "browserName": "chrome",
-#         "browserVersion": "122.0",
-#         "selenoid:options": {
-#             "enableVNC": True,
-#             "enableVideo": True
-#         }
-#     }
-#     options.capabilities.update(selenoid_capabilities)
-#     driver = webdriver.Remote(
-#         command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-#         options=options
-#     )
-#
-#     browser.config.window_height = 1080  # задает высоту окна браузера
-#     browser.config.window_width = 1920
-#
-#     browser.config.driver = driver
-#
-#     yield
-#     browser.quit()
-
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture
 def selenoid():
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "127.0",
+        "browserVersion": "122.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
         }
     }
-
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}",
-        options=options)
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser.config.window_height = 1080  # задает высоту окна браузера
+    browser.config.window_width = 1920
 
     browser.config.driver = driver
-    attach.add_html()
-    attach.add_logs()
-    attach.add_video()
-    attach.add_screenshot()
 
-
-@pytest.fixture(scope="session", autouse=True)
-def load_env():
-    load_dotenv()
-
-selenoid_login = os.getenv("SELENOID_LOGIN")
-selenoid_pass = os.getenv("SELENOID_PASS")
-selenoid_url = os.getenv("SELENOID_URL")
-
-
-@pytest.fixture(scope='session', autouse=True)
-def browser_manager():
-    driver_options = webdriver.ChromeOptions()
-    driver_options.page_load_strategy = 'eager'
-    browser.config.driver_options = driver_options
-    browser.config.base_url = "https://demoqa.com"
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
     yield
     browser.quit()
+
+# @pytest.fixture(scope="session", autouse=True)
+# def selenoid():
+#     options = Options()
+#     selenoid_capabilities = {
+#         "browserName": "chrome",
+#         "browserVersion": "127.0",
+#         "selenoid:options": {
+#             "enableVNC": True,
+#             "enableVideo": True
+#         }
+#     }
+#
+#     options.capabilities.update(selenoid_capabilities)
+#     driver = webdriver.Remote(
+#         command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}",
+#         options=options)
+#
+#     browser.config.driver = driver
+#     attach.add_html()
+#     attach.add_logs()
+#     attach.add_video()
+#     attach.add_screenshot()
+#
+#
+# @pytest.fixture(scope="session", autouse=True)
+# def load_env():
+#     load_dotenv()
+#
+# selenoid_login = os.getenv("SELENOID_LOGIN")
+# selenoid_pass = os.getenv("SELENOID_PASS")
+# selenoid_url = os.getenv("SELENOID_URL")
+#
+#
+# @pytest.fixture(scope='session', autouse=True)
+# def browser_manager():
+#     driver_options = webdriver.ChromeOptions()
+#     driver_options.page_load_strategy = 'eager'
+#     browser.config.driver_options = driver_options
+#     browser.config.base_url = "https://demoqa.com"
+#     browser.config.window_width = 1920
+#     browser.config.window_height = 1080
+#     yield
+#     browser.quit()
